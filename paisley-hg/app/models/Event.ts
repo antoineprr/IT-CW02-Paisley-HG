e9f0f4a90841433f236d4c84a67716d469b1065f
antoineprr <antoine.poirier@etu.imt-nord-europe.fr>
2025-12-05 13:38:24 +0000

    init

---
+++ paisley-hg/app/models/Event.ts
@@ -0,0 +1,19 @@
+import { Schema, model, models, Document } from "mongoose";
+
+export interface IEvent extends Document {
+  name: string;
+  category: string;
+  date: string;
+  location: string;
+  description?: string;
+}
+
+const EventSchema = new Schema<IEvent>({
+  name: { type: String, required: true },
+  category: String,
+  date: String,
+  location: String,
+  description: String
+}, { timestamps: true });
+
+export const Event = models.Event ?? model<IEvent>("Event", EventSchema);
---
+++ paisley-hg/app/routes.ts
@@ -0,0 +1,18 @@
+import type { RouteConfig } from "@react-router/dev/routes";
+import {
+  index,
+  layout,
+  route,
+} from "@react-router/dev/routes";
+
+export default [
+  layout("layouts/sidebar.tsx", [
+    index("routes/home.tsx"),
+    route("catalog", "routes/catalog.tsx"),
+    route("authors", "routes/authors.tsx"),
+    route("genres", "routes/genres.tsx"),
+    route("books", "routes/books.tsx"),
+    route("instances", "routes/bookinstances.tsx"),
+  ]),
+  route("about", "routes/about.tsx"),
+] satisfies RouteConfig;
